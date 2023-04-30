c64481456a7d0c9b30faf293d535ad3d4e648ddc
gthm1 <gthm298@gmail.com>
2023-04-30 20:10:04 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => 
+{
+  res.send('A Simple Node App!')
+}
+)
+
+app.listen(3000, () => 
+{
+  console.log('Server listening on port 3000')
+}
+)
